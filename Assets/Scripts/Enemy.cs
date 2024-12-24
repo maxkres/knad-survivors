6d2ea0dcdde32d98db1b3fdb0cfb787ad1389507
Konstantin Shaga <kvshaga@edu.hse.ru>
2024-12-24 16:29:07 +0300

    fixed player damage

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -6,9 +7,13 @@ public class Enemy : MonoBehaviour
     public int hp;
     public int maxHP;
     public int damage;
-    public float knockbackForce = 2f;
+    public float knockbackForce = 10f;
+
+    public float speed = 1f;
 
     private Transform player;
+
+    public float playerKnockbackThreshold = 0.2f;
 
     void Start()
     {
@@ -24,21 +29,37 @@ public class Enemy : MonoBehaviour
         if (player != null)
         {
             Vector3 direction = (player.position - transform.position).normalized;
-            transform.position += direction * Time.deltaTime;
+            transform.position += direction * speed * Time.deltaTime;
+        }
+
+        List<Collider2D> contacts = new();
+        GetComponent<CapsuleCollider2D>().GetContacts(contacts);
+
+        foreach(var collision in contacts) {
+            if (collision.CompareTag("Player")) {
+                if (collision.GetComponent<Rigidbody2D>().linearVelocity.magnitude <= playerKnockbackThreshold)
+                {
+                    PlayerStats.TakeDamage(damage);
+
+                    Vector3 knockbackDirection = (collision.transform.position - transform.position).normalized;
+                    collision.GetComponent<Rigidbody2D>().linearVelocity.Set(0, 0);
+                    collision.GetComponent<Rigidbody2D>().AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+                }
+            }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        UnityEngine.Debug.Log(collision);
-        if (collision.CompareTag("Player"))
-        {
-            PlayerStats.TakeDamage(damage);
+    // void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     Debug.Log(collision);
+    //     if (collision.CompareTag("Player") && collision.GetComponent<Rigidbody2D>().linearVelocity.Equals(Vector3.zero))
+    //     {
+    //         PlayerStats.TakeDamage(damage);
 
-            Vector3 knockbackDirection = (collision.transform.position - transform.position).normalized;
-            collision.GetComponent<Rigidbody2D>().AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
-        }
-    }
+    //         Vector3 knockbackDirection = (collision.transform.position - transform.position).normalized;
+    //         collision.GetComponent<Rigidbody2D>().AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+    //     }
+    // }
 
     public void TakeDamage(int amount)
     {
@@ -57,8 +78,7 @@ public class Enemy : MonoBehaviour
         hp = UnityEngine.Random.Range(10, 20) + PlayerStats.lvl * 2;
         damage = UnityEngine.Random.Range(5, 10) + PlayerStats.lvl;
 
-        // Лог для отладки
-        UnityEngine.Debug.Log($"Enemy initialized with HP: {hp}, Damage: {damage}");
+        // UnityEngine.Debug.Log($"Enemy initialized with HP: {hp}, Damage: {damage}");
     }
 
 }
